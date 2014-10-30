fa87d1fd30b174dc5b9b566e14355670a10e3754
Darbnīca <WokrRoom@HSs-MacBook-Pro.local>
2014-10-30 15:17:43 +0200

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Darbnīca on 30/10/14.
+//  Copyright (c) 2014 Skolnīca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Darbnīca on 30/10/14.
+//  Copyright (c) 2014 Skolnīca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
