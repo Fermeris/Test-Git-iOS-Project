7a4eb7d18487b0b4b109e31f7491bd908f98200c
Darbnīca <hstrazds@gmail.com>
2014-10-30 15:32:44 +0200

    added test class

---
+++ Git Test/TEst.h
@@ -0,0 +1,13 @@
+//
+//  TEst.h
+//  Git Test
+//
+//  Created by Darbnīca on 30/10/14.
+//  Copyright (c) 2014 Skolnīca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TEst : NSObject
+
+@end
---
+++ Git Test/TEst.m
@@ -0,0 +1,13 @@
+//
+//  TEst.m
+//  Git Test
+//
+//  Created by Darbnīca on 30/10/14.
+//  Copyright (c) 2014 Skolnīca. All rights reserved.
+//
+
+#import "TEst.h"
+
+@implementation TEst
+
+@end
